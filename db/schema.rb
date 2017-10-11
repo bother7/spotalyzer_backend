6fbca94866648693b070e699ff93cbc38cf42970
bother7 <josephmcha@gmail.com>
2017-10-11 14:23:40 -0400

    does this work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011174232) do
+ActiveRecord::Schema.define(version: 20171011180801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 20171011174232) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "access_token"
+    t.string "refresh_token"
   end
 
 end
