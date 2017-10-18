b6fcaa35aee122356579d1caab199fa560c33480
bother7 <josephmcha@gmail.com>
2017-10-17 20:07:46 -0400

    updating playlists work

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::PlaylistsController < ApplicationController
-  before_action :find_user_via_jwt, only: [:recent, :create, :destroy, :show, :my_playlists, :playlist_tracks]
+  before_action :find_user_via_jwt, except: []
+
+  def update
+    @playlist = Playlist.find_or_create_by({id: params[:id]})
+    playlist_array = params[:song_array].map do |song|
+      Song.find(song["id"])
+    end
+    if @playlist.songs.all? { |i| playlist_array.include?(i) } && @playlist.songs.length == playlist_array.length
+      update_playlist(playlist_array)
+    else
+      add_song = playlist_array - @playlist.songs
+      remove_song = @playlist.songs - playlist_array
+      if add_song.length > 0
+        add_song_to_playlist(add_song)
+      elsif remove_song.length > 0
+        remove_song_from_playlist(remove_song)
+      else
+      end
+    end
+  end
 
   def recent
     array = my_playlists
@@ -43,9 +62,7 @@ class Api::V1::PlaylistsController < ApplicationController
     render json: @playlists.where({display: true})
   end
 
-  def patch
 
-  end
 
 private
 
@@ -76,8 +93,8 @@ private
         @playlist.songs = []
         new_resp["items"].map do |song|
           @artist = Artist.find_or_create_by(name: song["track"]["artists"][0]["name"])
-          song = Song.find_or_create_by({title: song["track"]["name"], uri: song["track"]["uri"], artist: @artist})
-          @playlist.songs << song
+          thisSong = Song.find_or_create_by({title: song["track"]["name"], uri: song["track"]["uri"], artist: @artist})
+          @playlist.songs << thisSong
         end
         render json: @playlist.songs
       else
@@ -85,5 +102,62 @@ private
       end
   end
 
+  def update_playlist(playlist_array)
+    authorization_header = { 'Authorization' => "Bearer #{@user.updated_token}", 'Content-Type' => 'application/json' }
+    if @playlist.tracks_url.include?(@user.spotify_id)
+      url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists/#{@playlist.spotify_id}/tracks"
+      playlist_uri_array = playlist_array.map {|playlist| playlist.uri}
+      request_body = {uris: playlist_uri_array}.to_json
+      byebug
+      response = RestClient.put("#{url}", request_body, authorization_header)
+      new_resp = JSON.parse(response)
+        if new_resp["snapshot_id"]
+          @playlist.songs = playlist_array
+          render json: {snapshot_id: new_resp["snapshot_id"]}
+        else
+          render json: {status: 418}
+        end
+    else
+      render json: {status: 412, message: "cannot edit this playlist"}
+    end
+  end
+
+  def remove_song_from_playlist(song_array)
+    authorization_header = { 'Authorization' => "Bearer #{@user.updated_token}",  'Content-Type' => 'application/json'}
+    if @playlist.tracks_url.include?(@user.spotify_id)
+      url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists/#{@playlist.spotify_id}/tracks"
+      playlist_uri_array = song_array.map {|playlist| {"uri": playlist.uri, "position": [@playlist.songs.find_index(playlist)]}}
+      request_body = {tracks: playlist_uri_array}.to_json
+      response = RestClient::Request.execute(method: :delete, url: "#{url}", payload: request_body, headers: authorization_header)
+      new_resp = JSON.parse(response)
+        if new_resp["snapshot_id"]
+          @playlist.songs = @playlist.songs - song_array
+          render json: {snapshot_id: new_resp["snapshot_id"]}
+        else
+          render json: {status: 418}
+        end
+    else
+      render json: {status: 412, message: "cannot edit this playlist"}
+    end
+  end
+
+  def add_song_to_playlist(song_array)
+    authorization_header = { 'Authorization' => "Bearer #{@user.updated_token}",'Content-Type' => 'application/json' }
+    if @playlist.tracks_url.include?(@user.spotify_id)
+      url = "https://api.spotify.com/v1/users/#{@user.spotify_id}/playlists/#{@playlist.spotify_id}/tracks"
+      playlist_uri_array = song_array.map {|playlist| playlist.uri}
+      request_body = {uris: playlist_uri_array}.to_json
+      response = RestClient.post("#{url}", request_body, authorization_header)
+      new_resp = JSON.parse(response)
+        if new_resp["snapshot_id"]
+          @playlist.songs = [*@playlist.songs, *song_array]
+          render json: {snapshot_id: new_resp["snapshot_id"]}
+        else
+          render json: {status: 418}
+        end
+    else
+      render json: {status: 412, message: "cannot edit this playlist"}
+    end
+  end
 
 end
